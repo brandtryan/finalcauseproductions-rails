d6a0addd7fca59e233b41e32da9aadb177f8d268
brandtryan <emailme@brandtryan.com>
2024-05-19 13:39:10 -0400

    Add render.yaml

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-bundle exec rails db:migrate
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rails db:migrate
