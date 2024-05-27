74e04979e7a674349c294ab1282a39be685e61ad
brandtryan <emailme@brandtryan.com>
2024-05-27 11:37:21 -0400

    Finish 14.1.6

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_27_152148) do
     t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
