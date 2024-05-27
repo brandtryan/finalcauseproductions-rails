64c927befea572fd1d55426b116f5003e6cbd1a2
brandtryan <emailme@brandtryan.com>
2024-05-26 23:45:20 -0400

    Finish 10.4.2 FIXED

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -29,12 +30,6 @@ class UsersController < ApplicationController
   def edit
   end
 
-  def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
-    redirect_to users_url, status: :see_other
-  end
-
   def update
     if @user.update(user_params)
       flash[:success] = "Profile updated"
@@ -42,6 +37,12 @@ class UsersController < ApplicationController
     else
       render 'edit', status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url, status: :see_other
   end
 
   private
